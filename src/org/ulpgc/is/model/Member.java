508226c7bb4e002115a9492b8fef1d985aa563ec
Nestorpr <nesso2004@gmail.com>
2023-10-12 12:28:16 +0100

    Address and Member classes

--- src/org/ulpgc/is/model/Member.java
+++ src/org/ulpgc/is/model/Member.java
@@ -1,4 +1,22 @@
 package org.ulpgc.is.model;
 
-public class Member {
+public class Member extends Customer{
+	private int points;
+	private Address address;
+	public Member(int points, String name, String surname, NIF nif) {
+		super(name, surname, nif);
+		this.points = points;
+	}
+
+	public int getPoints() {
+		return points;
+	}
+
+	public void setPoints(int points) {
+		this.points = points;
+	}
+
+	public void setAddress(String street, int number,int postalCode, String city) {
+		this.address = new Address(street, number, postalCode, city);
+	}
 }
