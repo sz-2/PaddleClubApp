5145bbb9e96227f79f1d58d627689a0175520935
faridsanchez <s@>
2023-10-17 00:43:08 -0700

    Reservation methods

--- src/org/ulpgc/is/model/Reservation.java
+++ src/org/ulpgc/is/model/Reservation.java
@@ -1,17 +1,23 @@
 package org.ulpgc.is.model;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 
 public class Reservation {
 
+	private Customer customer;
+	private Court court;
+	private ArrayList<Extra> extra;
 	private static int NEXT_ID = 0;
 	private final int id;
 	private LocalDateTime date;
 
-	public Reservation(int id, LocalDateTime date) {
+	public Reservation(int id, LocalDateTime date, Customer customer, Court court) {
 		NEXT_ID++;
 		this.id = id;
 		this.date = date;
+		this.customer = customer;
+		this.court = court;
 	}
 
 	public int getId() {
@@ -24,6 +30,14 @@ public class Reservation {
 
 	public void setDate(LocalDateTime date) {
 		this.date = date;
+	}
+
+	public int price(){
+		return this.court.getPrice();
+	}
+
+	public void extra(Extra extra){
+		this.extra.add(extra);
 	}
 
 }
